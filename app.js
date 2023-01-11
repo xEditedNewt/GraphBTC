39447d4713d0b1d02113eebc5222cb975450ac2b
xEditedNewt <edited.newt@gmail.com>
2023-01-11 17:47:12 +0100

    tests

--- app.js
+++ app.js
@@ -63,6 +63,17 @@ const cosmosData = async () => {
     prices
   }
 }
+const bnbData = async () => {
+  const response = await fetch('https://min-api.cryptocompare.com/data/v2/histominute?fsym=BNB&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146');
+  const json = await response.json();
+  const data = json.Data.Data
+  const times = data.map(obj => obj.time)
+  const prices = data.map(obj => obj.high)
+  return {
+    times,
+    prices
+  }
+}
 
 
 const ethereumData = async () => {
@@ -172,7 +183,84 @@ async function printBtcChart() {
   });
 }
 
+async function printBnbChart() {
+  let { times, prices } = await bnbData()
+  let bnbChart = await document.getElementById('bnbChart').getContext('2d');
+  
+  let gradient = await bnbChart.createLinearGradient(0, 0, 0, 400);
 
+  gradient.addColorStop(0, 'rgba(247,147,26,.5)');
+  gradient.addColorStop(.425, 'rgba(255,193,119,0)');
+  Chart.defaults.global.defaultFontFamily = 'Red Hat Text';
+  Chart.defaults.global.defaultFontSize = 12;
+
+  bnbChartChart = new Chart(bnbChart, {
+    type: 'line',
+    data: {
+      labels: times,
+      datasets: [{
+        label: '$',
+        data: prices,
+        backgroundColor: gradient,
+        borderColor: 'rgba(247,147,26,1)',
+        borderJoinStyle: 'round',
+        borderCapStyle: 'round',
+        borderWidth: 3,
+        pointRadius: 0,
+        pointHitRadius: 10,
+        lineTension: .2,
+      }]
+    },
+
+    options: {
+      title: {
+        display: false,
+        text: 'Heckin Chart!',
+        fontSize: 35
+      },
+
+      legend: {
+        display: false
+      },
+
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      },
+
+      scales: {
+        xAxes: [{
+          display: false,
+          gridLines: {}
+        }],
+        yAxes: [{
+          display: false,
+          gridLines: {}
+        }]
+      },
+
+      tooltips: {
+        callbacks: {
+          //This removes the tooltip title
+          title: function() {}
+       },
+        //this removes legend color
+        displayColors: false,
+        yPadding: 10,
+        xPadding: 10,
+        position: 'nearest',
+        caretSize: 10,
+        backgroundColor: 'rgba(255,255,255,.9)',
+        bodyFontSize: 15,
+        bodyFontColor: '#303030' 
+      }
+    }
+  });
+}
 
 async function printCosmosChart() {
   let { times, prices } = await cosmosData()
@@ -192,7 +280,7 @@ async function printCosmosChart() {
     data: {
       labels: times,
       datasets: [{
-        label: "",
+        label: "$",
         data: prices,
         backgroundColor: gradient,
         borderColor: 'rgba(46,49,72,1)',
@@ -359,9 +447,19 @@ async function updateBitcoinPrice() {
 
   document.getElementById("btcPrice").innerHTML = "$" + currentPrice;
 }
+async function updateBnbPrice() {
+  let { times, prices } = await bnbData()
+  let currentPrice = prices[prices.length-1].toFixed(2);
+
+  document.getElementById("bnbPrice").innerHTML = "$" + currentPrice;
+}
 
 updateBitcoinPrice()
 updateCosmosPrice()
+updateEthereumPrice()
+updateBnbPrice()
 
 printBtcChart()
 printCosmosChart()
+printEthereumChart()
+printBnbChart()
